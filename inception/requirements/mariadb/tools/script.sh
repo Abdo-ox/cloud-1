b909ac5b44ef28cbcf223d70c5a4a630d56c3baf
Abdo-ox <abderrahim.jari6@gmail.com>
2025-12-25 09:24:03 +0100

    fix the issue with the db

--- inception/requirements/mariadb/tools/script.sh
+++ inception/requirements/mariadb/tools/script.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-if [ ! -f /var/lib/mysql/password_changed ]; then
-    echo "the first run ################"
-    service mariadb start
-    until mysqladmin ping --socket=/run/mysqld/mysqld.sock --silent; do
-        sleep 1
-    done
-    echo "the first run ################1"
-    mysql -e " CREATE DATABASE IF NOT EXISTS ${SQL_NAME};
-    CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON ${SQL_NAME}.* TO '${SQL_USER}'@'%';
-    FLUSH PRIVILEGES;"
-    echo "the first run ################2"
-    mysqladmin shutdown
-    echo "the first run ################3"
-    touch /var/lib/mysql/password_changed
-fi
+mysqld_safe &
+pid="$!"
 
-exec mysqld_safe
+until mysqladmin ping --socket=/run/mysqld/mysqld.sock --silent; do
+    sleep 1
+done
 
+mysql -e " CREATE DATABASE IF NOT EXISTS ${SQL_NAME};
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${SQL_NAME}.* TO '${SQL_USER}'@'%';
+FLUSH PRIVILEGES;"
 
-
+echo "mariadb is running."
+wait "$pid"
--- inception/requirements/wordpress/tools/script.sh
+++ inception/requirements/wordpress/tools/script.sh
@@ -2,6 +2,8 @@
 #download worpress files
 
 wp core install --allow-root --path="$WP_PATH" --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_EMAIL"
-wp user create --allow-root --path="$WP_PATH" "$WP_USERNAME" "$WP_EMAIL" --role=subscriber --user_pass="$WP_USERPASS"
-
+if ! wp user get "$WP_USERNAME" --allow-root --path="$WP_PATH" >/dev/null 2>&1; then
+    wp user create --allow-root --path="$WP_PATH" "$WP_USERNAME" "$WP_EMAIL" --role=subscriber --user_pass="$WP_USERPASS"
+fi
+stdbuf -oL echo "wordpress is running"
 exec php-fpm7.4 -F
