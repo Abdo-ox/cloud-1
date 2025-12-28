e958944522680b809fe4990d163d4c0539815a57
ajari <ajari@debian.myguest.virtualbox.org>
2025-12-28 13:35:54 +0100

    change the folder scructer and separate the container to run indeveually for more flixibility

--- inception/requirements/wordpress/tools/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#download worpress files
-
-wp core install --allow-root --path="$WP_PATH" --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_EMAIL"
-if ! wp user get "$WP_USERNAME" --allow-root --path="$WP_PATH" >/dev/null 2>&1; then
-    wp user create --allow-root --path="$WP_PATH" "$WP_USERNAME" "$WP_EMAIL" --role=subscriber --user_pass="$WP_USERPASS"
-fi
-stdbuf -oL echo "wordpress is running"
-exec php-fpm7.4 -F
--- playbooks/roles/db/tasks/files/mariadb/tools/script.sh
+++ playbooks/roles/db/tasks/files/mariadb/tools/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "run mariadb script"
 mysqld_safe &
 pid="$!"
 
@@ -7,7 +7,7 @@ until mysqladmin ping --socket=/run/mysqld/mysqld.sock --silent; do
     sleep 1
 done
 
-mysql -e " CREATE DATABASE IF NOT EXISTS ${SQL_NAME};
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_NAME};
 CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${SQL_NAME}.* TO '${SQL_USER}'@'%';
 FLUSH PRIVILEGES;"
