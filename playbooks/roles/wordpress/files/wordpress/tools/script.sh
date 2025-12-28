0c8baebe43b5863705c720b07598de4094d62c78
ajari <ajari@debian.myguest.virtualbox.org>
2025-12-28 14:27:02 +0100

    fix all the issue with separated services/ now everything works just need to be runned on a new instance to comfirm that everything is fine

---
+++ playbooks/roles/wordpress/files/wordpress/tools/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#download worpress files
+echo "run wordpress script"
+wp core install --allow-root --path="$WP_PATH" --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_EMAIL"
+if ! wp user get "$WP_USERNAME" --allow-root --path="$WP_PATH" >/dev/null 2>&1; then
+    wp user create --allow-root --path="$WP_PATH" "$WP_USERNAME" "$WP_EMAIL" --role=subscriber --user_pass="$WP_USERPASS"
+fi
+stdbuf -oL echo "wordpress is running"
+touch /ready
+exec php-fpm7.4 -F
